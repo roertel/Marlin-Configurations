c45a7ad530fdbd387a2527eabd5f6e17f04f2bad
Ryan Oertel <Ryan.Oertel+Git@gmail.com>
2025-01-29 13:36:35 -0600

    feat: reduce beeping

--- config/examples/Creality/Ender-3 Pro/CrealityV422/Configuration_adv.h
+++ config/examples/Creality/Ender-3 Pro/CrealityV422/Configuration_adv.h
@@ -2685,7 +2685,7 @@
   #define FILAMENT_UNLOAD_PURGE_FEEDRATE      25  // (mm/s) feedrate to purge before unload
 
   #define PAUSE_PARK_NOZZLE_TIMEOUT           45  // (seconds) Time limit before the nozzle is turned off for safety.
-  #define FILAMENT_CHANGE_ALERT_BEEPS         10  // Number of alert beeps to play when a response is needed.
+  #define FILAMENT_CHANGE_ALERT_BEEPS         3   // Number of alert beeps to play when a response is needed.
   #define PAUSE_PARK_NO_STEPPER_TIMEOUT           // Enable for XYZ steppers to stay powered on during filament change.
   //#define FILAMENT_CHANGE_RESUME_ON_INSERT      // Automatically continue / load filament when runout sensor is triggered again.
   //#define PAUSE_REHEAT_FAST_RESUME              // Reduce number of waits by not prompting again post-timeout before continuing.
